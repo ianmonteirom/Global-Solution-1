36db0190c6bdc47c27aa19664f1c31b692659585
Ian <ianmonteiromoreira@gmail.com>
2024-06-01 18:29:54 -0300

    Avançando com o projeto

--- Front e Web/src/js/script.js
+++ Front e Web/src/js/script.js
@@ -1,1 +1,27 @@
 // alert('Seja bem-vindo!') 
+
+// SlideShow Automático
+let imagens=['./src/assets/pesca1.jpg','./src/assets/pesca2.jpg','./src/assets/pesca3.jpg'];
+let index = 0;
+let time = 3000;
+
+function slideShow(){
+    document.getElementById('imgBanner').src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index = 0;
+    }
+    setTimeout('slideShow()', time);
+}
+slideShow();
+
+function clickMenu(){
+    if(item.style.display == 'block') {
+        item.style.display ='block'
+    } else {
+        item.style.display = 'block'
+
+    }
+
+}
